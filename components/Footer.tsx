f6282dff87b705d85f3c6c3baf862adca23b9d91
legendholding192025 <mufeed.rahman@legendholding.com>
2025-08-21 15:54:53 +0400

    social-icons-added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
@@ -28,16 +29,18 @@ const Footer = () => {
             <h4 className="text-lg font-semibold mb-4 font-subhead text-zul-green">Quick Links</h4>
             <ul className="space-y-2">
               {[
-                'Home', 'Why Us', 'Solutions', 'Capabilities', 
-                'Certifications', 'Case Studies', 'Our Clients', 'Contact'
+                { name: 'R&D', href: '/rd' },
+                { name: 'About', href: '/about' },
+                { name: 'News', href: '/news' },
+                { name: 'Contact', href: '/contact' }
               ].map((link) => (
-                <li key={link}>
-                  <a
-                    href={`#${link.toLowerCase().replace(' ', '-')}`}
+                <li key={link.name}>
+                  <Link
+                    href={link.href}
                     className="text-zul-grey-light hover:text-zul-yellow transition-colors font-body"
                   >
-                    {link}
-                  </a>
+                    {link.name}
+                  </Link>
                 </li>
               ))}
             </ul>
@@ -84,7 +87,9 @@ const Footer = () => {
             <div className="flex space-x-4">
               {/* Facebook */}
               <a
-                href="#"
+                href="https://www.facebook.com/share/19KowfbS7s/"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="w-10 h-10 bg-zul-grey-dark rounded-full flex items-center justify-center hover:bg-zul-green transition-colors"
                 aria-label="Facebook"
               >
@@ -95,7 +100,9 @@ const Footer = () => {
               
               {/* X (Twitter) */}
               <a
-                href="#"
+                href="https://x.com/ZulEnergy?s=09"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="w-10 h-10 bg-zul-grey-dark rounded-full flex items-center justify-center hover:bg-zul-green transition-colors"
                 aria-label="X (Twitter)"
               >
@@ -106,12 +113,27 @@ const Footer = () => {
               
               {/* YouTube */}
               <a
-                href="#"
+                href="https://www.youtube.com/@zulenergy2618"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="w-10 h-10 bg-zul-grey-dark rounded-full flex items-center justify-center hover:bg-zul-green transition-colors"
                 aria-label="YouTube"
               >
                 <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+                </svg>
+              </a>
+
+              {/* LinkedIn */}
+              <a
+                href="https://www.linkedin.com/company/zul-energy/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 bg-zul-grey-dark rounded-full flex items-center justify-center hover:bg-zul-green transition-colors"
+                aria-label="LinkedIn"
+              >
+                <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
                 </svg>
               </a>
             </div>
