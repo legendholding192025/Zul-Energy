89d1959af940eaa97b12a01dec04ce8e426d55a3
legendholding192025 <mufeed.rahman@legendholding.com>
2025-09-04 16:18:06 +0400

    new-fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section id="home" className="relative text-white mt-24">
+    <section id="home" className="relative text-white mt-24 sm:min-h-[calc(100svh-6rem)] lg:min-h-[calc(100vh-6rem)]">
       {/* Background Video */}
       <video
         className="absolute inset-0 w-full h-full object-cover"
@@ -15,7 +15,7 @@ const Hero = () => {
 
       {/* Content */}
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 sm:py-32 md:py-40 lg:py-56">
-        <div className="text-center sm:text-right">
+        <div className="text-center sm:text-left">
           <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-headline mb-0 text-white font-bold">
             CRAFTSMANSHIP
           </h1>
